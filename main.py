2faae5deaf1cf3b65c7eae64030dfbb615ed0316
Davide Sanvito <dsanvito90@gmail.com>
2017-12-18 01:52:29 +0100

    Implemented stoppable threads

--- main.py
+++ main.py
@@ -9,46 +9,61 @@ from pprint import pprint
 import logging
 
 
-def polling_thread():
-    while True:
-        # Re-route intents as soon as TM_TRAINING_SET_SIZE have been collected
-        if len(statsManager.get_tm_store()) > 0 and len(statsManager.get_tm_store()) % TM_TRAINING_SET_SIZE == 0:
-            reroute_event.set()
-        threading.Thread(target=statsManager.poll_stats).start()
-        time.sleep(POLLING_INTERVAL)
-        if kill_threads[0]:
-            reroute_event.set()
-            return
+class PollingThread(threading.Thread):
+    def __init__(self):
+        self._stopevent = threading.Event()
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while not self._stopevent.isSet():
+            # Re-route intents as soon as TM_TRAINING_SET_SIZE TMs have been collected
+            if len(statsManager.get_tm_store()) > 0 and len(statsManager.get_tm_store()) % TM_TRAINING_SET_SIZE == 0:
+                reroute_event.set()
+            threading.Thread(target=statsManager.poll_stats).start()
+            time.sleep(POLLING_INTERVAL)
+
+    def stop(self):
+        self._stopevent.set()
 
 
-def rerouting_thread():
-    while True:
-        reroute_event.wait()
-        if kill_threads[0]:
-            return
-        logging.info("Re-routing Intents...")
-        reroute_event.clear()
-        
-        # Get the latest version of topology and monitored events
-        imrManager.retrieve_monitored_intents_from_ONOS()
-        topoManager.retrieve_topo_from_ONOS()
-        
-        # Build the worst case Traffic Matrix by keeping only demands whose intentKeys are still monitored and
-        # considering their worst-case historical value of the latest training interval
-        tm_list = statsManager.get_tm_store()[-TM_TRAINING_SET_SIZE:]
-        monitored_intents = imrManager.get_monitored_intents()
-        worst_tm = {intentKey: max([tm[intentKey] if intentKey in tm else 0 for tm in tm_list])
-                    for intentKey in monitored_intents}
+class ReroutingThread(threading.Thread):
+    def __init__(self):
+        self._stopevent = threading.Event()
+        threading.Thread.__init__(self)
 
-        if set(sum([tm.keys() for tm in tm_list], [])) != monitored_intents:
+    def run(self):
+        while not self._stopevent.isSet():
+            reroute_event.wait()
+            if self._stopevent.isSet():
+                return
+
+            logging.info("Re-routing Intents...")
+            reroute_event.clear()
+
+            # Get the latest version of topology and monitored events
+            imrManager.retrieve_monitored_intents_from_ONOS()
+            topoManager.retrieve_topo_from_ONOS()
+
+            # Build the worst case Traffic Matrix by keeping only demands whose intentKeys are still monitored and
+            # considering their worst-case historical value of the latest training interval
+            tm_list = statsManager.get_tm_store()[-TM_TRAINING_SET_SIZE:]
+            monitored_intents = imrManager.get_monitored_intents()
+            worst_tm = {intentKey: max([tm[intentKey] if intentKey in tm else 0 for tm in tm_list])
+                        for intentKey in monitored_intents}
+
+            if set(sum([tm.keys() for tm in tm_list], [])) != monitored_intents:
+                if verbose:
+                    print 'Some tm samples, related to intents which are no longer monitored, have been ignored...'
+
             if verbose:
-                print 'Some tm samples, related to intents which are no longer monitored, have been ignored...'
+                print 'worst_tm'
+                pprint({flow_id: bps_to_human_string(worst_tm[flow_id]) for flow_id in worst_tm})
 
-        if verbose:
-            print 'worst_tm'
-            pprint({flow_id: bps_to_human_string(worst_tm[flow_id]) for flow_id in worst_tm})
+            imrManager.reroute_monitored_intents(worst_tm, topoManager)
 
-        imrManager.reroute_monitored_intents(worst_tm, topoManager)
+    def stop(self):
+        self._stopevent.set()
+
 
 if __name__ == "__main__":
     verbose = True
@@ -59,15 +74,21 @@ if __name__ == "__main__":
     statsManager = StatsManager(verbose)
 
     imrManager = IMRManager(verbose)
-    
+
     reroute_event = threading.Event()
 
     kill_threads = [False]
-    
-    threading.Thread(target=polling_thread).start()
-    threading.Thread(target=rerouting_thread).start()
 
-    logging.info('Press any key to exit...')
+    pollingThread = PollingThread()
+    reroutingThread = ReroutingThread()
+    pollingThread.start()
+    reroutingThread.start()
+
+    logging.info('Press any key to exit')
+
     raw_input('')
+
     logging.info('Killing all the threads...')
-    kill_threads = [True]
+    pollingThread.stop()
+    reroutingThread.stop()
+    reroute_event.set()
